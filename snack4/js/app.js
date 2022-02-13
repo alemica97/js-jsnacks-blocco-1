ccdef85924ea2bfbf3f494c17dcf6d5068acf02e
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-13 21:03:03 +0100

    snack4

---
+++ snack4/js/app.js
@@ -0,0 +1,31 @@
+//-------------------------------------Snack-4---------------------------
+
+//creo un Array con la lista degli invitati
+const gatsbyParty = [
+    'Tom',
+    'Nick',
+    'Daisy',
+    'Myrtle',
+    'Jordan',
+]
+console.log(gatsbyParty);
+
+//salvo il nome dell'utente in una variabile
+const userName = prompt('Ciao, come ti chiami?');
+console.log(userName);
+
+let correctName = false;
+
+for(let i = 0; i < gatsbyParty.length; i++){
+    if(gatsbyParty[i] === userName){
+        alert(`Ei ${userName}, sei il benvenuto alla festa di Gatsby!!`);
+        correctName = true;
+        break;
+    }else{
+
+    }
+}
+
+if(correctName == false){
+    alert(`Mi spiace ${userName} non sei invitato alla festa!!`);
+}
